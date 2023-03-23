f98fdbdddcbe6ed738d1c89e9af5284171a9f8bd
Maximiliano Romero Budib <maximilianoromerobudib@yahoo.com.mx>
2023-03-23 00:39:05 +0000

    Tarea 2 finalizada

--- retoAnalitica/tarea2.py
+++ retoAnalitica/tarea2.py
@@ -5,11 +5,20 @@ import seaborn as sns
 # %%
 df = pd.read_csv("/workspace/retoAnalitica/data/ulabox_orders_with_categories_partials_2017.csv")
 df.tail()
-# %%
+#%%
 df.describe()
+#%%
+df.info()
 # %%
 df.sample()
 # %%
 #Categorica y numerica
 #continua y discreta 
 #ordinal y nominal 
+# %%
+df.std()
+# %%
+df[df['total_items'] < 2]
+# %%
+df[df['discount%'] < 0]
+# %%
