5dd92263a0e8aa245cd0dc6e957205d70d95535b
OshkarVTec <a01275287@tec.mx>
2023-03-23 21:01:17 -0600

    Descuento contra total de items

--- retoAnalitica/reto.py
+++ retoAnalitica/reto.py
@@ -6,5 +6,8 @@ import seaborn as sns
 df = pd.read_csv("../data/ulabox_orders_with_categories_partials_2017.csv")
 df.tail()
 
-
+# %%
+#Descuento contra total de items.
+sns.scatterplot(data=df1, x="discount%", y="total_items")
+#No parece que haya una relacion importante
 # %%
