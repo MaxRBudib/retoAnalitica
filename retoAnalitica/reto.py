99b3b8413d737b5c887ccb43536c51509a953fae
OshkarVTec <a01275287@tec.mx>
2023-03-23 21:08:23 -0600

    Compras por hora

--- retoAnalitica/reto.py
+++ retoAnalitica/reto.py
@@ -16,3 +16,7 @@ sns.displot(x = 'weekday', data = df)
 #Hay más compras en el día 1, y disminuyen hasta el día 6, para
 #aumentar de nuevo el día 7
 # %%
+#Compras por hora
+sns.displot(x = 'hour', data = df)
+#La mayoría de las compras se hacen a medio día y después de las 10
+# %%
